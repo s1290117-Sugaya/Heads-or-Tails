0c683e665c72bee63791baed13222b87dda9e95b
s1290117-Sugaya <s1290117@u-aizu.ac.jp>
2022-07-12 16:21:36 +0900

    HoT: initial revision

---
+++ index.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class index{
+	int count_h=0, count_t=0;
+
+	public String which(){
+		Random rand = new Random();
+		if(rand.nextInt(2) == 1){
+			count_h++;
+			return "Heads";
+		}else{
+			count_t++;
+			return "Tails";
+		}
+	}
+	public void coinToss(){
+		System.out.println("Tossing a coin...");
+		for(int i=1; i<=3; i++){
+			System.out.println("Round "+i+": "+which());
+		}
+		
+		System.out.println("Heads: "+count_h+", Tails: "+count_t);
+	}
+
+	public static void main(String[] args){
+		index i = new index();
+		i.coinToss();
+	}
+}
